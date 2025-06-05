c581ddaf2f70c3c156265acb107c638722a4ce3b
Kajal Mishra <kajalmishraa345@gmail.com>
2025-06-05 11:42:34 +0530

    Add files via upload

---
+++ Day-1/js/script.js
@@ -0,0 +1,69 @@
+/**
+ *single line comment
+ */
+
+ /**
+  * 
+  * multiline comment 
+  */
+
+ /**  print message on console  */
+ console.log("Hello World")
+
+ /*print message on webhpage */
+ document.write("Hello World welcome to javascript session")
+
+ /**display text within color */
+ document.write("<font color=red><br> Hello! welcome to the javascript session-2</font>")
+ document.write("<font color=blue><br> Hello!  welcome to the javascript session-2</font>")
+ document.write("<font color=purple><br> Hello welcome to the javascript session-2</font>")
+ document.write("<font color=brown><br> Hello welcome to the javascript session-2</font>")
+ document.write("<font color=green><br> Hello welcome to the javascript session-2</font>")
+
+ /*display alert message */ 
+ alert("Hello !welcome to the javascript session-2")
+ window.alert("<font color=purpole><br> Hello welcome to the javascript session-2</font>")
+
+ /*what is variable?*/
+ /* variable is a contrainer that can be store data that can be used later according to the requirement */
+ a=4;
+ b=6;
+ console.log(a+b);  /* output 10*/
+
+ Hello ="<font color=purple><br> Hello welcome to the javascript session-2</font>"
+ document.write(Hello);
+
+ MyName = "Kajal Mishra"; /** outpur (Kajal Mishra ) */
+ console.write(myName)
+
+
+
+ /*  -------------------------self -work-------------------------------- */
+
+ document.write("Hello Madam")
+
+ document.write("<font color=Gray></font><br> Hello Welcome to My Website!!......")
+ document.write("<font color=brown><br>This Website help you to get some skills<font>")
+ 
+
+ alert("YOU HAVE AN ERROR ")
+ window.alert("<font color=purpole><br>  THIS IS MY WEBSITE </font>")
+
+
+ a=9;
+ b=14;
+ console.log(a*b)
+
+/* var (var is function scope variable ,var is used for declaring variable in old javascript )*/
+ var a = 10;
+
+ //let ,cons (introduction in javascript  e s6 varsion in 2015)
+ //let and const are block scope variables
+ //let and const are used for declaring variable in modern javascript 
+ let b =20;
+ let c= 30;
+
+
+ /take input from user
+ var name = prompt("Enter your name");
+ console.log(name); //output : name entered by user 
